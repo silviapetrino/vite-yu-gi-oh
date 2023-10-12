823b52605d268d35c5b336122c6e1a494098293b
silviapetrino <petrino.silvia@gmail.com>
2023-10-12 16:10:08 +0200

    [add] offset random

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 
 <script>
+
 import Header from './components/Header.vue';
 import ContainerCards from './components/ContainerCards.vue';
+import { store } from './data/store';
+import axios from 'axios';
 
   export default {
     name: "AppVue",
     components: {
       Header,
       ContainerCards
+    },
+    data(){
+      return{
+        store,
+        offset: Math.floor(Math.random() * 12810) + 1,
+        apiUrlWithOffset : `https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=${this.offset}`
+      }
+    },
+    methods: {
+
+      getApi(){
+        axios.get(store.apiUrl)
+        store.apiUrl = this.apiUrlWithOffset
+        .then((result) => {
+          store.cardList = result.data
+        })
+      }
+    },
+
+    mounted(){
+      this.getApi()
+      console.log(this.offset)
     }
   }
 </script>
